3271b95a18999d1c608c55d908b280afcb4cc83e
GiancarloRamondino <giancarlo.ramondino@gmail.com>
2025-04-14 16:25:30 +0200

    [UPD] name fuction

--- name.js
+++ name.js
@@ -1,1 +1,6 @@
-  
+const Name=() => {
+  const firstNames = ["Alice", "Bob", "Charlie", "David", "Eve"];
+    const lastName = ["Smith", "Johnson", "Williams", "Jones", "Brown"];
+  return firstNames[Math.floor(Math.random() * firstNames.length)] + " " + lastName[Math.floor(Math.random() * lastName.length)];
+}
+console.log(Name());
